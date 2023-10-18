be7f99496783d83f7eb73b8baf82b503fea9c2c5
jodle001 <60989139+jodle001@users.noreply.github.com>
2023-10-18 15:38:37 -0400

    added some documentation and error checking for when pyads cannot read or write to the PLC.

--- ros2_pyads/ads_com.py
+++ ros2_pyads/ads_com.py
@@ -1,9 +1,16 @@
 import pyads
+from rclpy.impl import rcutils_logger
 
 
 class AdsCom:
 
     def __init__(self, com_config, plc_admin):
+        """
+        Initialize the ADS communication object.
+
+        :param com_config: Object containing the ADS communication configuration YAML data
+        :param plc_admin: Object containing the PLC admin credentials YAML data
+        """
         self.com_config = com_config
         self.plc_admin = plc_admin
 
@@ -18,10 +25,17 @@ class AdsCom:
         self.plc_admin_user = self.plc_admin['plc_admin_user']
         self.plc_admin_pass = self.plc_admin['plc_admin_pass']
 
+        # Get logger
+        self.logger = rcutils_logger.RcutilsLogger(name='ads_com')
+
         # Initialize the route
         self.initialize_route()
 
     def initialize_route(self):
+        """
+        Initialize the route to the PLC using the sender AMS ID, PLC IP, route name, host name, and PLC admin
+        credentials.
+        """
         pyads.open_port()
         pyads.set_local_address(self.sender_ams)
         pyads.add_route_to_plc(
@@ -34,10 +48,40 @@ class AdsCom:
         pyads.close_port()
 
     def read_by_name(self, var_name, var_type):
-        with pyads.Connection(self.remote_ads, pyads.PORT_TC3PLC1, self.plc_ip) as plc:
-            var = plc.read_by_name(var_name, var_type)
-        return var
+        """
+        Read a variable from the PLC by name.
+
+        :param var_name: The name of the variable to read (e.g., 'MAIN.testVar')
+        :param var_type: The type of the variable to read (e.g., pyads.PLCTYPE_DWORD)
+
+        :return: The value of the variable
+        """
+        try:
+            with pyads.Connection(self.remote_ads, pyads.PORT_TC3PLC1, self.plc_ip) as plc:
+                var = plc.read_by_name(var_name, var_type)
+            return var
+        except pyads.pyads_ex.ADSError as e:
+            if self.logger:
+                self.logger.error(e.msg)
+            else:
+                print(e)
+            return None
 
     def write_by_name(self, var_name, var_value, var_type):
-        with pyads.Connection(self.remote_ads, pyads.PORT_TC3PLC1, self.plc_ip) as plc:
-            plc.write_by_name(var_name, var_value, var_type)
+        """
+        Write a variable to the PLC by name.
+
+        :param var_name: The name of the variable to write (e.g., 'MAIN.testVar')
+        :param var_value: The value of the variable to write (e.g., 5)
+        :param var_type: The type of the variable to write (e.g., pyads.PLCTYPE_DWORD)
+        """
+        try:
+            with pyads.Connection(self.remote_ads, pyads.PORT_TC3PLC1, self.plc_ip) as plc:
+                plc.write_by_name(var_name, var_value, var_type)
+            return True
+        except pyads.pyads_ex.ADSError as e:
+            if self.logger:
+                self.logger.error(e.msg)
+            else:
+                print(e)
+            return False
--- ros2_pyads/ads_com_node.py
+++ ros2_pyads/ads_com_node.py
@@ -12,7 +12,7 @@ class ADSComNode(Node):
                          allow_undeclared_parameters=True,
                          automatically_declare_parameters_from_overrides=True)
 
-        # Get the parameters
+        # Get the parameters from the launch file
         com_config = self.get_parameter('com_config').value
         plc_admin = self.get_parameter('plc_admin').value
 
@@ -27,18 +27,44 @@ class ADSComNode(Node):
         with open(com_config, 'r') as file:
             com_config_data = yaml.safe_load(file)
 
+        # Load the PLC admin data from the YAML file
         with open(plc_admin, 'r') as file:
             plc_admin_data = yaml.safe_load(file)
 
         # Initialize the ADS communication object
         self.ads_com = AdsCom(com_config_data, plc_admin_data)
 
-        self.ads_com.write_by_name('MAIN.testVar', 5, pyads.PLCTYPE_DWORD)
+        new_value = 10
+
+        # Example write call to the PLC
+        success = self.ads_com.write_by_name('MAIN.testVar', new_value, pyads.PLCTYPE_DWORD)
+
+        b_success = self.ads_com.write_by_name(
+            var_name='MAIN.bTestThing',
+            var_value=not self.ads_com.read_by_name('MAIN.bTestThing', pyads.PLCTYPE_BOOL),  # Toggle the existing value
+            var_type=pyads.PLCTYPE_BOOL)
+
+        if not success:
+            self.get_logger().error('Failed to write variable to PLC')
+
+        if not b_success:
+            self.get_logger().error('Failed to write variable to PLC')
+
+        # Example read call from the PLC
+        var = self.ads_com.read_by_name('MAIN.testVar', pyads.PLCTYPE_DWORD)
+
+        if var is None:
+            self.get_logger().error('Failed to read variable from PLC')
+        elif var != new_value:
+            self.get_logger().error('Read variable from PLC does not match written value')
+        else:
+            self.get_logger().info('Successfully wrote and read variable from PLC')
 
 
 def main():
     rclpy.init()
     node = ADSComNode()
+    # rclpy.spin(node)
     rclpy.shutdown()
 
 
