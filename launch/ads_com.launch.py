2599ff20a33959044c6f66a4176bf550f35714bf
jodle001 <60989139+jodle001@users.noreply.github.com>
2023-10-18 15:02:37 -0400

    Fixed launch and setup files
    Now this is verified to communicate with Beckhoff PLC

--- launch/ads_com.launch.py
+++ launch/ads_com.launch.py
@@ -1,9 +1,8 @@
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
@@ -11,39 +10,26 @@ def generate_launch_description():
 
     return LaunchDescription([
         # Declare launch arguments
-        DeclareLaunchArgument('sim', default_value='true', description='Simulation flag'),
-        DeclareLaunchArgument('pub_rate', default_value='10', description='Rate of topic publisher (Hz)'),
-        DeclareLaunchArgument('config', default_value='example/eip_config_2.yaml', description='Path to EIP config file'),
-        DeclareLaunchArgument('ip', default_value='192.168.0.1', description='IP address of PLC'),
+        DeclareLaunchArgument(
+            name='com_config',
+            default_value='config/com_config.yaml',
+            description='Path to ADS Configuration file'),
+        DeclareLaunchArgument(
+            name='plc_admin',
+            default_value='config/plc_admin.yaml',
+            description='Path to Admin Configuration file'),
 
         # EIP bridge simulator node (if simulation is true)
         GroupAction([
             Node(
-                package='eip_bridge',
-                executable='eip_bridge_simulator_node',
+                package='ros2_pyads',
+                executable='ads_com_node',
                 # name='eip_bridge_simulator',
                 output='screen',
                 parameters=[
-                    {'pub_rate': LaunchConfiguration('pub_rate')},
-                    {'config': PathJoinSubstitution([package_path, LaunchConfiguration('config')])}
+                    {'com_config': PathJoinSubstitution([package_path, LaunchConfiguration('com_config')])},
+                    {'plc_admin': PathJoinSubstitution([package_path, LaunchConfiguration('plc_admin')])}
                 ],
-                condition=IfCondition(LaunchConfiguration('sim'))
             )
         ]),
-
-        # EIP bridge node (if simulation is false)
-        GroupAction([
-            Node(
-                package='eip_bridge',
-                executable='eip_bridge_node',
-                # name='eip_bridge',
-                output='screen',
-                parameters=[
-                    {'ip': LaunchConfiguration('ip')},
-                    {'pub_rate': LaunchConfiguration('pub_rate')},
-                    {'config': PathJoinSubstitution([package_path, LaunchConfiguration('config')])}
-                ],
-                condition=UnlessCondition(LaunchConfiguration('sim'))
-            )
-        ])
     ])
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'example'), glob('config/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
